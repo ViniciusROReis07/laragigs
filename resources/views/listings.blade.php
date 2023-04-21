b5f6d9f75db96bc11024d932076078f732aaa30e
ViniciusROReis07 <guaguinhireis@gmail.com>
2023-04-21 15:35:15 -0300

    feat: validating if the array is empty

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,5 +1,15 @@
 <h1>{{$heading}}</h1>
 
+{{-- @php
+    $number = 1
+@endphp --}}
+
+{{-- {{$number}} --}}
+
+@if(count($listings) == 0)
+    <p>No listings found</p>
+@endif
+
 @foreach($listings as $listing)
     <h2>{{$listing['title']}}</h2>
     <p>{{$listing['description']}}</p>
