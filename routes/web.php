05603fd9e2d710398fb4d3d8aa3816e561da1fad
ViniciusROReis07 <guaguinhireis@gmail.com>
2023-04-21 00:25:11 -0300

    feat: Query params implementation

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,8 +21,21 @@ Route::get('/', function () {
 Route::get('/hello', function(){
     return response('<h1>Hello World</h1>', 200)
     ->header('Content-Type', 'text/plain')
-    ->header('foo', 'bar')
-    ;
+    ->header('foo', 'bar');
 });
+// Podemos personalizar o header do response
 
+Route::get('/posts/{id}', function(int $id){
+    // dd($id);
+    // dd mata o programa nessa linha e serve para debugar
+    // ddd($id);
+    // ddd faz o mesmo que o dd mas exibe todo o codigo e a linha que parou
+    return response('Post '.$id);
+})->where('id', '[0-9]+');
+// Podemos criar condicores para acesso as rotas, caso nao atenda retornara 404
+
+Route::get('/search', function(Request $request){
+    // dd($request);
+    return $request->name. ' '. $request->city;
+});
 
