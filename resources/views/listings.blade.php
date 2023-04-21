60507b6b59587931970c172c11c60842313013b5
ViniciusROReis07 <guaguinhireis@gmail.com>
2023-04-21 15:39:56 -0300

    feat: using unless directive

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -5,12 +5,17 @@
 @endphp --}}
 
 {{-- {{$number}} --}}
-
+{{-- 
 @if(count($listings) == 0)
     <p>No listings found</p>
-@endif
+@endif --}}
 
-@foreach($listings as $listing)
-    <h2>{{$listing['title']}}</h2>
-    <p>{{$listing['description']}}</p>
-@endforeach
+{{-- A diretiva @unless em Laravel é usada para executar um bloco de código se a expressão fornecida como argumento for falsa. --}}
+@unless (count($listings) == 0)
+    @foreach($listings as $listing)
+        <h2>{{$listing['title']}}</h2>
+        <p>{{$listing['description']}}</p>
+    @endforeach
+@endunless
+    
+
