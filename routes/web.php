1dfb4e973db9184e733fdebb8c4e046624fffd80
ViniciusROReis07 <guaguinhireis@gmail.com>
2023-04-21 00:39:19 -0300

    style: clearing routes

--- routes/web.php
+++ routes/web.php
@@ -15,27 +15,27 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings');
 });
 
-Route::get('/hello', function(){
-    return response('<h1>Hello World</h1>', 200)
-    ->header('Content-Type', 'text/plain')
-    ->header('foo', 'bar');
-});
+// Route::get('/hello', function(){
+//     return response('<h1>Hello World</h1>', 200)
+//     ->header('Content-Type', 'text/plain')
+//     ->header('foo', 'bar');
+// });
 // Podemos personalizar o header do response
 
-Route::get('/posts/{id}', function(int $id){
-    // dd($id);
-    // dd mata o programa nessa linha e serve para debugar
-    // ddd($id);
-    // ddd faz o mesmo que o dd mas exibe todo o codigo e a linha que parou
-    return response('Post '.$id);
-})->where('id', '[0-9]+');
+// Route::get('/posts/{id}', function(int $id){
+//     // dd($id);
+//     // dd mata o programa nessa linha e serve para debugar
+//     // ddd($id);
+//     // ddd faz o mesmo que o dd mas exibe todo o codigo e a linha que parou
+//     return response('Post '.$id);
+// })->where('id', '[0-9]+');
 // Podemos criar condicores para acesso as rotas, caso nao atenda retornara 404
 
-Route::get('/search', function(Request $request){
-    // dd($request);
-    return $request->name. ' '. $request->city;
-});
+// Route::get('/search', function(Request $request){
+//     // dd($request);
+//     return $request->name. ' '. $request->city;
+// });
 
