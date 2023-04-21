d495e593fa9759b97b6a1f5686d7dd77fe036e03
ViniciusROReis07 <guaguinhireis@gmail.com>
2023-04-21 15:28:30 -0300

    style: cleaning interpolation with blade

--- resources/views/listings.php
+++
@@ -1,9 +0,0 @@
-<h1>
-    <?php 
-        echo($heading);
-    ?>
-    <?php foreach($listings as $listing): ?>
-        <h2><?php echo $listing['title'];  ?></h2>
-        <h2><?php echo $listing['description']; ?></h2>
-    <?php endforeach; ?>
-</h1>
