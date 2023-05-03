85898deeb96ad8d4e0cf2646b38c5a18bdf6a3ad
ViniciusROReis07 <guaguinhireis@gmail.com>
2023-05-03 16:46:33 -0300

    fix: change to component

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -42,7 +42,7 @@
         {{-- View output --}}
         {{-- Essa diretiva é usada para definir uma seção de conteúdo que pode ser preenchida por uma visualização (view) que estende o layout que contém essa diretiva. --}}
             <main>
-                @yield('content')
+                {{$slot}}
             </main>    
         <footer class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center">
             <p class="ml-2">Copyright &copy; 2023, All Rights reserved</p>
